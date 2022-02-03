e0a54cdd09e8f6ec7aa1d2de06a741df9348184f
konejin <konejin@gmail.com>
2022-02-03 13:38:52 -0600

    created battery class

--- python_practice/inheritance/electric_car.py
+++ python_practice/inheritance/electric_car.py
@@ -39,14 +39,35 @@ class Car:
             print("You can't roll back an odometer.")
 
 
+class Battery:
+    """A simple attempt to model a battery for an electric car"""
+    
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size"""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+
 class ElectricCar(Car):
     """Represents aspect of a car, specific to electric values"""
 
     def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then, initialize attributes specific to an electric car.
+        """
         super().__init__(make, model, year)
+        self.battery = Battery()
+
 
 
 my_tesla = ElectricCar("Tesla", "Model S", "2022")
 print(my_tesla.get_descriptive_name())
+
+my_tesla.battery.describe_battery()
+
 # print(my_tesla.odometer_reading)  # subclass will inherit ALL attributes
 # from superclass, including those whose parameters are not defined
