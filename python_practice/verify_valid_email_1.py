9c0e986cde6dbdbbdbeaa32e75a1dfa3eed4bb67
konejin <konejin@gmail.com>
2022-02-02 23:22:28 -0600

    fixed class distribution and logic to verify email format

--- python_practice/verify_valid_email_1.py
+++ python_practice/verify_valid_email_1.py
@@ -2,38 +2,26 @@ import re
 
 
 class VerifyEmail:
-    '''Verifies if an email is valid'''
-    user_input = input(f"Type email you want to verify and press enter\n")
+    '''Verifies if an email provided by user is valid'''
 
-    def __init__(self, search_pattern, user_input) -> str:
+    def __init__(self, email) -> str:
+        """
+        Initialize attributes. Defines the search pattern using regular
+        expressions. email parameter will be taken by input at the moment
+        of creating class intance.
+        """
+        self.email = email
         self.search_pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
-        
+        # regex pattern is simplified here for educational purposes
+
     def valid_format(self):
         '''Verifies if an email is in the correct format'''
-        print(self.user_input)
-        # print(f"{self.search_pattern}")
+        if (re.search(self.search_pattern, self.email)):
+            print("valid email format")
+        else:
+            print("invalid email format")
+        print(f"Verified email is: {self.email}")
 
 
-# class IsValidEmail:
-#     "verifies if an email has a valid format"
-#     user_input = input(f"Type email you want to verify and press enter\n")
-
-#     def __init__(self) -> str:
-#         self.search_pattern = "[a-zA-Z0-9]+@[a-zA-Z]+\.(com|edu|net)"
-
-
-#     def validate_mail(self) -> str:
-#         # self.valid = f"{IsValidEmail.user_input} is a valid email"
-#         # self.invalid = 
-#         if(re.search(self.search_pattern, IsValidEmail.user_input)):
-#             print("valid")
-#         else:
-#             print("invalid")
-
-
-email = VerifyEmail.valid_format("alex@gmail.com")
-print(type(email))
-print(email)
-
-# if __name__ == '__main__':
-#     IsValidEmail.validate_mail(IsValidEmail.user_input)
+user_email = VerifyEmail(input("what is the email you want to verify?\n"))
+user_email.valid_format()
